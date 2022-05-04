d29f5b43cb5fef5ca01f15d68fd4e4e7b5dad5a5
Pauline Soubrié <pauline_soubrie@hotmail.fr>
2022-05-04 09:21:22 +0200

    add basket

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import { useEffect, useState } from "react";
 import logo from "./assets/images/deliveroo-logo.png";
 
 function App() {
+  //creation of states to catch data
   const [data, setData] = useState({});
   const [isLoading, setIsLoading] = useState(true);
-
+  // request for data with useEffect, just once at the first opening of the page
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -22,24 +23,16 @@ function App() {
     fetchData();
   }, []);
 
-  const getMeals = (index) => {
-    return data.categories[index].meals.map((item) => {
-      return (
-        <div key={item.id} className="component">
-          <div className="left-side">
-            <h3>{item.title}</h3>
-            <p>{item.description}</p>
-            <span>{item.price} €</span>
-          </div>
-          {item.picture && <img alt={item.title} src={item.picture} />}{" "}
-        </div>
-      );
-    });
-  };
+  // creation of basket's state
+  const [basket, setBasket] = useState([]);
+  const [subtotal, setSubtotal] = useState(0);
+  const frais = 2.5;
 
+  //waiting for request to arrive before coninuing
   return isLoading ? (
     <span>Loading...</span>
   ) : (
+    // front end of deliveroo using data
     <div className="App">
       <header>
         <div className="logo-header">
@@ -59,19 +52,120 @@ function App() {
       </header>
       <main>
         <div>
-          <div className="left-column">
+          <div className="meals-column">
+            {/* map on data array to get all titles */}
             {data.categories.map((category, index) => {
               return (
+                // we dont keep categories with zero meals
                 data.categories[index].meals.length > 0 && (
                   <section key={index}>
                     <h2>{category.name}</h2>
-                    <div className="componentsOfMeal">{getMeals(index)}</div>
+                    <div className="all-meals">
+                      {/* each meal can be clicked to put it in the basket */}
+                      {category.meals.map((meal) => {
+                        return (
+                          <div
+                            key={meal.id}
+                            className="meal"
+                            // when clicking :
+                            onClick={() => {
+                              if (
+                                // if this meal is still in basket :
+                                basket.find((item) => item.id === meal.id) ===
+                                undefined
+                              ) {
+                                // we add a new object in basket state with meal title, id, price and quantity
+                                meal.quantity = 1;
+                                const newBasket = [...basket];
+                                newBasket.push(meal);
+                                setBasket(newBasket);
+                                setSubtotal(subtotal + Number(meal.price));
+                              } else {
+                                // else we add a quantity in basket state object with this id
+                                const newBasket = [...basket];
+                                console.log(newBasket);
+                                newBasket.find(
+                                  (item) => item.id === meal.id
+                                ).quantity += 1;
+                                setBasket(newBasket);
+                                setSubtotal(subtotal + Number(meal.price));
+                              }
+                            }}
+                          >
+                            {/* another part of front end */}
+                            <div className="left-side">
+                              <h3>{meal.title}</h3>
+                              <p>{meal.description}</p>
+                              <span>{meal.price} €</span>
+                            </div>
+                            {meal.picture && (
+                              <img alt={meal.title} src={meal.picture} />
+                            )}{" "}
+                          </div>
+                        );
+                      })}
+                    </div>
                   </section>
                 )
               );
             })}
           </div>
-          <div className="right-column">Panier</div>
+          {/* what is shown in basket : */}
+          <div className="basket">
+            <div className="submit">Valider votre panier</div>
+            <div className="order">
+              {/* map on basket state to show all ordered meals */}
+              {basket.map((item) => {
+                return (
+                  <div className="basket-orders" key={item.id}>
+                    {" "}
+                    {/* function handlePlus and handleMinus to increment/decreent counters  */}
+                    <button
+                      onClick={() => {
+                        const newBasket = [...basket];
+                        const thisMeal = newBasket.find(
+                          (meal) => meal.id === item.id
+                        );
+                        if (thisMeal.quantity > 1) {
+                          thisMeal.quantity -= 1;
+                          setBasket(newBasket);
+                          setSubtotal(subtotal - thisMeal.price);
+                        } else {
+                          newBasket.splice(thisMeal, 1);
+                          setBasket(newBasket);
+                          setSubtotal(subtotal - thisMeal.price);
+                        }
+                      }}
+                    >
+                      -
+                    </button>
+                    <p>{item.quantity}</p>
+                    <button
+                      onClick={() => {
+                        const newBasket = [...basket];
+                        const thisMeal = newBasket.find(
+                          (meal) => meal.id === item.id
+                        );
+                        thisMeal.quantity += 1;
+                        setBasket(newBasket);
+
+                        setSubtotal(subtotal + Number(thisMeal.price));
+                      }}
+                    >
+                      +
+                    </button>
+                    <p>{item.title}</p>
+                    <p>{item.price}</p>
+                  </div>
+                );
+              })}
+            </div>
+            <div className="sub-total">
+              <p>Sous-total : {subtotal}</p>
+              <p>Frais de livraison : {frais}</p>
+            </div>
+            <div className="total">Total : {subtotal + frais}</div>
+          </div>
         </div>
       </main>
     </div>
